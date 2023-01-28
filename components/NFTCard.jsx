172dbea2ce3008a970b17abdba21058aeb248bb1
Petar-23 <50322343+Petar-23@users.noreply.github.com>
2023-01-28 11:22:38 +0100

    NFT Marketplce Section Coded

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,7 +1,60 @@
 'use client';
 
-const NFTCard = () => {
-  <div>NFTCard</div>;
-};
+const NFTCard = () => (
+  <div className="flex flex-col h-[648px] w-[439px] rounded-3xl nft-glassmorphism rotate-x">
+    <div className="relative mt-5 mr-5 ml-5">
+      <img
+        src="/26_anim.gif"
+        className="rounded-[20px]"
+      />
+      <div className="absolute top-3 right-3">
+        <div className="flex flex-row justify-between gap-2 rounded-full backdrop-blur-xl py-1 px-3">
+          <img
+            src="heart-solid.svg"
+            className="w-[33px] object-contain"
+          />
+          <p className="font-semibold text-[24px] leading-8 text-SDX-200">120</p>
+        </div>
+      </div>
+      <div className="absolute bottom-3 left-3">
+        <div className="flex rounded-full backdrop-blur-xl py-3 px-3">
+          <p className="font-light text-[16px] leading-6 text-SDX-200">Guardians of SwapDEX</p>
+        </div>
+      </div>
+    </div>
+    <div className="flex flex-col mr-5 ml-5 mt-5">
+      <div className="flex flex-row justify-between">
+        <div className=" flex flex-row gap-3">
+          <img
+            src="/clock-solid.svg"
+            className="object-contain w-[28px]"
+          />
+          <p className="font-light text-[16px] leading-5 text-SDX-200 my-auto">
+            15h : 23m : 12s
+          </p>
+        </div>
+        <div className=" flex flex-row gap-3">
+          <img
+            src="/tag-solid.svg"
+            className="object-contain w-[28px]"
+          />
+          <p className="font-light text-[16px] leading-5 text-SDX-200 my-auto">
+            299 USDT
+          </p>
+        </div>
+      </div>
+      <p className="mt-6 font-bold text-[32px] leading-8 text-SDX-200">DIGITAL DEFENDER</p>
+      <p className="mt-6 font-light text-[16px] leading-5 text-SDX-200">Properties</p>
+      <div className="flex flex-row gap-3 mt-3">
+        <div className="rounded-full py-2 px-3 bg-SDX-700">
+          <p className="font-light text-[16px] leading-5 text-SDX-200">SDX Ecosystem NFT</p>
+        </div>
+        <div className="rounded-full py-2 px-3 bg-SDX-700">
+          <p className="font-light text-[16px] leading-5 text-SDX-200">#23</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default NFTCard;
--- sections/NFTMarketplace.jsx
+++ sections/NFTMarketplace.jsx
@@ -1,8 +1,65 @@
 'use client';
 
+import styles from '../styles';
+import NFTCard from '../components/NFTCard';
+
 const NFTMarketplace = () => (
-  <section>
-    NFTMarketplace
+  <section className={`${styles.yPaddings} w-full h-screen bg-SDX-900`}>
+    <div className="flex flex-col h-full w-full">
+      <div className={`flex flex-col ${styles.innerWidth} mx-auto`}>
+        <p className="font-semibold text-[18px] leading-6 text-center text-SDX-Pink">NFT MARKETPLACE</p>
+        <h1 className="font-bold text-[40px] leading-[48px] text-SDX-200 text-center my-2">Discover rare NFT collections <br /> on our cross-chain NFT marketplace</h1>
+        <p className="font-light text-[18px] leading-6 text-center text-SDX-200">Buy, sell, auction and create NFTs today!</p>
+      </div>
+      <div className={`grid grid-cols-2 ${styles.innerWidth} mx-auto mt-10`}>
+        <div className="relative">
+          <div className="absolute top-10 left-0 bottom-0">
+            <img src="/hero_ball_01_2.png" className="blur-md z-0 -translate-x-24" />
+          </div>
+          <NFTCard />
+        </div>
+        <div className="flex flex-col w-full h-full">
+          <div className="relative">
+            <div className="absolute top-0 left-0 right-0 bottom-0 z-0 m-5">
+              <img src="/hero_ball_02_1.png" className="blur-xl z-0" />
+            </div>
+            <div className="flex flex-row justify-end gap-5 z-10">
+              <div className="flex flex-col z-10">
+                <p className="font-bold text-[32px] leading-10 text-SDX-200">1000+</p>
+                <p className="font-regular text-[18px] leading-6 text-SDX-200">Artworks</p>
+              </div>
+              <div className="flex flex-col z-10">
+                <p className="font-bold text-[32px] leading-10 text-SDX-200">50+</p>
+                <p className="font-regular text-[18px] leading-6 text-SDX-200">Collections</p>
+              </div>
+            </div>
+            <div className="lex w-full rounded-[17px] mt-10 glassmorphism px-2 pt-2 z-10">
+              <div className="flex flex-col bg-SDX-900 w-full rounded-[12px]">
+                <div className="flex flex-row ml-3 gap-2 my-2">
+                  <div className="h-3 w-3 rounded-full bg-SDX-Pink" />
+                  <div className="h-3 w-3 rounded-full bg-amber-500" />
+                  <div className="h-3 w-3 rounded-full bg-green-500" />
+                </div>
+                <img src="/NFT-marketplace-showcase.svg" />
+              </div>
+            </div>
+
+            <div className="flex flex-row justify-between mt-10">
+              <img src="/coingecko-seeklogo-gray.svg" className="z-10" />
+              <img src="/coinmarketcap-logo-gray.svg" className="z-10" />
+            </div>
+            <div className="flex flex-row justify-between mt-4">
+              <img src="/polygon-matic-logo-gray.svg" className="h-[50px] object-contain z-10" />
+              <img src="/bnb-bnb-logo-gray.svg" className="h-[50px] object-contain z-10" />
+              <img src="/swapdex-logo-gray.svg" className="h-[50px] object-contain z-10" />
+              <img src="/eth-logo-gray.svg" className="h-[50px] object-contain z-10" />
+              <img src="/solana-sol-logo-gray.svg" className="h-[50px] object-contain z-10" />
+            </div>
+          </div>
+        </div>
+        <p>Test</p>
+      </div>
+    </div>
   </section>
 );
 
