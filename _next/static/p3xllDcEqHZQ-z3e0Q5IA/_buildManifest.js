933a3d9226ec0117711eb8a63fc02343c4238f3b
Petar-23 <Petar-23@users.noreply.github.com>
2023-04-12 17:58:48 +0000

    deploy: 351b6244d34f97c73f2722e037ef508a45dbff14

---
+++ _next/static/p3xllDcEqHZQ-z3e0Q5IA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/922edf34-0e10d25a399ffbfb.js","static/chunks/188-ce159010aee864ff.js","static/chunks/pages/index-046501a43a1fd941.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
