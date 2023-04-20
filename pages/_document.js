c18a267bddc98c9fcaee0a759c34921787696bc1
Petar-23 <50322343+Petar-23@users.noreply.github.com>
2023-04-20 19:35:45 +0200

    removed cookie script

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,6 @@ class MyDocument extends Document {
       <Html lang="en" className="overflow-x-hidden">
         <Head>
           {/* Add any additional Head elements here */}
-          <script
-            type="text/javascript"
-            src="//cdn.cookie-script.com/s/66761a5566442f8ed0337fe12e4fd83f.js"
-          />
         </Head>
         <body className="overflow-x-hidden overscroll-x-none">
           <Main />
